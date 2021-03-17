cba6cd0326bd6ff5f997a2f997ffccaef5c5c2c7
axetroy <axetroy.dev@gmail.com>
2021-03-17 14:36:32 +0800

    警告策略

---
+++ alarm.go
@@ -0,0 +1,64 @@
+package watchdog
+
+import (
+	"sync"
+	"time"
+)
+
+// 限流的报警器，不至于过于频繁的报警
+// 定义一个报警策略
+type Alarm struct {
+	mux               sync.Mutex
+	interval          time.Duration // 触发两次报警的间隔时间
+	lastTriggerAt     *time.Time    // 上一次触发时间
+	triggerTimesToDay uint          // 今日已触发次数
+	maxTriggerPerDate uint          // 每天最多触发次数，0 无限次数
+}
+
+func NewAlarm(interval time.Duration, maxTriggerPerDate uint) *Alarm {
+	return &Alarm{
+		interval:          interval,
+		maxTriggerPerDate: maxTriggerPerDate,
+	}
+}
+
+func (a *Alarm) ShouldTrigger() (shouldTrigger bool) {
+	a.mux.Lock()
+	defer a.mux.Unlock()
+
+	defer func() {
+		if shouldTrigger {
+			t := time.Now()
+
+			// 如果是同一天
+			if a.lastTriggerAt != nil && t.Day() == a.lastTriggerAt.Day() {
+				a.triggerTimesToDay = a.triggerTimesToDay + 1
+			} else {
+				a.triggerTimesToDay = 1
+			}
+
+			a.lastTriggerAt = &t
+
+		}
+	}()
+
+	if a.lastTriggerAt == nil {
+		shouldTrigger = true
+		return
+	}
+
+	now := time.Now()
+
+	// 如果超出了当前报警的量
+	if now.Day() == a.lastTriggerAt.Day() && a.triggerTimesToDay >= a.maxTriggerPerDate {
+		return false
+	}
+
+	// 如果还没有到下一次的报警时间
+	if now.After(a.lastTriggerAt.Add(a.interval)) {
+		shouldTrigger = true
+		return
+	}
+
+	return
+}
---
+++ alarm_test.go
@@ -0,0 +1,56 @@
+package watchdog
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAlarm(t *testing.T) {
+	{
+		alarm := NewAlarm(time.Microsecond*500, 20) // 间隔 0.5s，每天最多触发次数 20 次
+
+		index := 0
+		for {
+			if index == 20 {
+				break
+			}
+
+			assert.True(t, alarm.ShouldTrigger(), index)
+			assert.Equal(t, int(uint(index+1)), int(alarm.triggerTimesToDay), index)
+
+			index++
+
+			time.Sleep(time.Microsecond * 600)
+		}
+
+		assert.False(t, alarm.ShouldTrigger(), index)
+		assert.Equal(t, 20, int(alarm.triggerTimesToDay), index)
+
+		assert.False(t, alarm.ShouldTrigger(), index)
+		assert.Equal(t, 20, int(alarm.triggerTimesToDay), index)
+
+		assert.False(t, alarm.ShouldTrigger(), index)
+		assert.Equal(t, 20, int(alarm.triggerTimesToDay), index)
+	}
+
+	{
+		alarm := NewAlarm(time.Millisecond*200, 20) // 间隔 2s，每天最多触发次数 20 次
+
+		index := 0
+		for {
+			if index == 20 {
+				break
+			}
+
+			index++
+
+			_ = alarm.ShouldTrigger()
+
+			time.Sleep(time.Millisecond * 100)
+		}
+
+		assert.Equal(t, 10, int(alarm.triggerTimesToDay), index)
+	}
+}
