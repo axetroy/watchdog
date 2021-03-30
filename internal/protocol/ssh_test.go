8021c5a3de071d5d128ae178c7b0d196ec2c94a1
axetroy <axetroy.dev@gmail.com>
2021-03-30 16:49:07 +0800

    test: update ssh test

--- internal/protocol/ssh_test.go
+++ internal/protocol/ssh_test.go
@@ -13,9 +13,12 @@ import (
 	gossh "golang.org/x/crypto/ssh"
 )
 
-func CreateSSHServer(addr string, cb func(c *ssh.Server)) error {
+func CreateSSHServer(addr string, cb func(c *ssh.Server) error) error {
 	server := &ssh.Server{
 		Addr: ":2222",
+		PasswordHandler: func(ctx ssh.Context, password string) bool {
+			return ctx.User() == "test" && password == "test"
+		},
 	}
 
 	server.Handle(func(s ssh.Session) {
@@ -34,50 +37,80 @@ func CreateSSHServer(addr string, cb func(c *ssh.Server)) error {
 
 	defer server.Close()
 
-	cb(server)
-
-	return nil
+	return cb(server)
 }
 
 func TestPingSSH(t *testing.T) {
 	type args struct {
 		addr string
+		auth interface{}
 	}
 	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-		listen  bool
+		name   string
+		args   args
+		err    string
+		listen bool
 	}{
 		{
 			name: "2222",
 			args: args{
 				addr: "localhost:2222",
+				auth: map[string]string{
+					"username": "test",
+					"password": "test",
+				},
 			},
-			wantErr: false,
-			listen:  true,
+			err:    "",
+			listen: true,
 		},
 		{
-			name: "2223",
+			name: "2222#wrong auth",
+			args: args{
+				addr: "localhost:2222",
+				auth: map[string]string{
+					"username": "wrong_username",
+					"password": "wrong_password",
+				},
+			},
+			err:    "ssh: handshake failed: ssh: unable to authenticate, attempted methods [none password], no supported methods remain",
+			listen: true,
+		},
+		{
+			name: "2222#without password",
+			args: args{
+				addr: "localhost:2222",
+			},
+			err:    "",
+			listen: true,
+		},
+		{
+			name: "2223 if port aviable",
 			args: args{
 				addr: "localhost:2223",
 			},
-			wantErr: true,
-			listen:  false,
+			err:    "dial tcp [::1]:2223: connect: connection refused",
+			listen: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if tt.listen == true {
-				err := CreateSSHServer(tt.args.addr, func(s *ssh.Server) {
-					err := PingSSH(context.Background(), tt.args.addr, nil)
-					assert.Equal(t, tt.wantErr, err != nil, tt.name)
+				err := CreateSSHServer(tt.args.addr, func(s *ssh.Server) error {
+					return PingSSH(context.Background(), tt.args.addr, tt.args.auth)
 				})
 
-				assert.Equal(t, tt.wantErr, err != nil, tt.name)
+				if tt.err == "" {
+					assert.Nil(t, err, tt.name)
+				} else {
+					assert.EqualError(t, err, tt.err, tt.name)
+				}
 			} else {
-				err := PingSSH(context.Background(), tt.args.addr, nil)
-				assert.Equal(t, tt.wantErr, err != nil, tt.name)
+				err := PingSSH(context.Background(), tt.args.addr, tt.args.auth)
+				if tt.err == "" {
+					assert.Nil(t, err, tt.name)
+				} else {
+					assert.EqualError(t, err, tt.err, tt.name)
+				}
 			}
 
 		})
